e4c87f354f42f8b4f7b06b0f732ec71c78c60708
hemfreitas11 <hemfreitas11@users.noreply.github.com>
2021-08-03 17:15:31 -0300

    Added Hex/RGB color support. Changed config loading to happen when the plugin starts.

--- src/main/java/me/bkrmt/bkhash/game/PlayGameRequest.java
+++ src/main/java/me/bkrmt/bkhash/game/PlayGameRequest.java
@@ -5,7 +5,6 @@ import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import static me.bkrmt.bkhash.BkHash.plugin;
@@ -60,7 +59,7 @@ public class PlayGameRequest {
                                 tpaMessage.addExtra(" ");
                                 temp2 = new StringBuilder();
                             } else {
-                                temp2.append(ChatColor.translateAlternateColorCodes('&', temp1[c] + " "));
+                                temp2.append(Utils.translateColor(temp1[c] + " "));
                             }
 
                             if (c == temp1.length - 1) {
